b0d578ca5804b1866b41aae6de8da3b1ad0cd137
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-06 18:18:21 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -21,3 +21,23 @@ class Solution {
         
     }
 }
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for (int i=1;i<ratings.length;i++) {
+            if (ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        int sum=candies[n-1];
+        for (int i=n-2;i>=0;i--) {
+            if (ratings[i] > ratings[i+1]) {
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
+            }
+            sum = sum + candies[i];
+        }
+        return sum;
+    }
+}
