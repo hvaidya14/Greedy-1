e71284972048bc2929297aa0ca85cb50f078cac9
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-06 17:32:15 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,19 @@
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int destination = nums.length-1;
+        for (int i = n-2;i>=0;i--) {
+            if (nums[i] + i >= destination){
+                destination = i;
+            }
+        }
+        if (destination == 0) {
+            return true;
+        }
+        return false;
+        
+    }
+}
